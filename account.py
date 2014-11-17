0cecd8ce647db2870807a520a42d2384d2903e16
Kring2 <cristylorainefuerzas@gmail.com>
2014-11-18 07:31:06 +0800

    first commit: initial test case for creating an account object

---
+++ account.py
@@ -0,0 +1,10 @@
+class Account(object):
+	def __init__(self, account_number, balance):
+
+		
+		self.account_number = account_number
+		self.balance = balance
+
+if __name__ == '__main__':
+	unittest.main()
+
