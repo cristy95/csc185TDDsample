e72d5e7dddc50531cab9c91e99437bc446f02039
Kring2 <cristylorainefuerzas@gmail.com>
2014-11-18 07:44:12 +0800

    added a new test case: for adding an account to the Bank

--- bank.py
+++ bank.py
@@ -2,6 +2,11 @@ class Bank(object):
 	def __init__(self):
 		self.accounts = {}
 
+	def add_account(self, account):
+		if account.balance == "hey":
+			return "Wrong Parameter"
+		else:
+			self.accounts[account.account_number] = account.balance
 	
 if __name__ == '__main__':
 	unittest.main()
--- bank_test.py
+++ bank_test.py
@@ -8,7 +8,16 @@ class BankTest(unittest.TestCase):
     self.assertEqual({}, bank.accounts)
     self.assertEqual(len(bank.accounts), 0)
 
- 
+ def test_add_account(self):
+  	bank = Bank()
+
+  	account_1 = Account(001, 50)
+  	account_2 = Account(002, 100)
+
+  	bank.add_account(account_1)
+  	bank.add_account(account_2)
+
+  	self.assertEqual(len(bank.accounts), 2)
   
 
 if __name__ == '__main__':
